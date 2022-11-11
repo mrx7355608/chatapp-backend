b23543e292cafe0bf0ca6f4af5e2939235f1d6c5
mrx7355608 <mrx7355608@gmail.com>
2022-11-12 02:24:28 +0500

    hide secrets in .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export default {
-    PORT: 4000,
-    DATABASE_URL:
-        "mongodb+srv://mern-chatapp:dbAdmin123@mern-chatapp.9d6bc7h.mongodb.net/?retryWrites=true&w=majority",
-    NODE_ENV: "development",
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
 };
