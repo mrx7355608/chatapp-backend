e01f198313a13c56213c48caea0c4ccd498d1e8e
mrx7355608 <mrx7355608@gmail.com>
2022-11-12 02:26:24 +0500

    fixed database url type

--- src/utils/databaseConnection.ts
+++ src/utils/databaseConnection.ts
@@ -2,7 +2,7 @@ import config from "@config/index";
 import mongoose from "mongoose";
 
 const connectDB = () => {
-    mongoose.connect(config.DATABASE_URL, () =>
+    mongoose.connect(config.DATABASE_URL as string, () =>
         console.log("[INFO] Connected to database")
     );
     mongoose.connection.on("error", () =>
