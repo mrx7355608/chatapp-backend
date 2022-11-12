38faabafbf09b6c0ba2a05546c0cee291ee924c3
mrx7355608 <mrx7355608@gmail.com>
2022-11-12 22:39:31 +0500

    created validation schemas for signup and login

---
+++ src/api/auth/auth.validations.ts
@@ -0,0 +1,42 @@
+import joi from "joi";
+
+const signupSchema = joi.object({
+    fname: joi.string().min(4).max(20).required().messages({
+        "string.empty": "First name cannot be empty",
+        "string.min": "First name should be at least 4 characters long",
+        "string.max": "First name cannot exceed 20 characters length",
+        "any.required": "Ummmm... don't you have a first name?",
+    }),
+    lname: joi.string().min(4).max(20).required().messages({
+        "string.empty": "Last name cannot be empty",
+        "string.min": "Last name should be at least 4 characters long",
+        "string.max": "Last name cannot exceed 20 characters length",
+        "any.required": "Ummmm... don't you have a last name?",
+    }),
+    username: joi.string().min(8).max(50).required().messages({
+        "string.empty": "Username cannot be empty",
+        "string.min": "Username should be at least 8 characters long",
+        "string.max": "Username should not exceed 50 characters length",
+        "any.required": "Username is missing",
+    }),
+    password: joi.string().min(8).required().messages({
+        "string.empty": "Password cannot be empty",
+        "string.min": "Password should be at least 8 characters long",
+        "any.required": "Password is missing",
+    }),
+    confirm_password: joi.valid(joi.ref("password")).messages({
+        "any.only": "Passwords do not match",
+    }),
+});
+
+const loginSchema = joi.object({
+    username: joi.string().required().messages({
+        "string.base": "Username should be a string",
+        "any.required": "Username is missing",
+    }),
+    password: joi.string().required().messages({
+        "string.base": "Password should be a string",
+        "any.required": "Password is missing",
+    }),
+});
+export { signupSchema, loginSchema };
